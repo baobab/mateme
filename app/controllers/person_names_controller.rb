05368c54207d704818ee388050d465cabe151be2
Jeff Rafter <jeff@baobabhealth.org>
2008-08-21 05:37:44 +0200

    Removing debug

--- app/controllers/person_names_controller.rb
+++ app/controllers/person_names_controller.rb
@@ -9,6 +9,6 @@ class PersonNamesController < ApplicationController
 
   def search(field_name, search_string)
     @names = PersonNameCode.find_most_common(field_name, search_string).collect{|person_name| person_name.send(field_name)}
-    render :text => "<li>Current: " + ((search_string || '').soundex || '') + "</li><li>" + @names.map{|n| (n + " - " + n.soundex) if n } .join("</li><li>") + "</li>"
+    render :text => "<li>" + @names.map{|n| n } .join("</li><li>") + "</li>"
   end
 end
