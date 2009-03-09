2f06c779ef08f92fdf4d947134dee8c577bb2933
dave gamphani phiri <gamphani@kafyontha.bao.bab>
2009-03-09 10:11:03 +0200

    Return error message if report start date is greater than report end date

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,8 +3,12 @@ class ReportController < ApplicationController
   def weekly_report
     @start_date = Date.new(params[:start_year].to_i,params[:start_month].to_i,params[:start_day].to_i) rescue nil
     @end_date = Date.new(params[:end_year].to_i,params[:end_month].to_i,params[:end_day].to_i) rescue nil
-
-  @diagnoses = ConceptName.find(:all,
+    if @start_date > @end_date
+      flash[:notice] = 'Start date is greater that end date'
+      redirect_to :action => 'select'
+      return
+    end
+    @diagnoses = ConceptName.find(:all,
                                   :joins =>
                                         "INNER JOIN obs ON
                                          concept_name.concept_id = obs.value_coded",
@@ -12,7 +16,7 @@ class ReportController < ApplicationController
                                             @start_date, @end_date],
                                   :group =>   "name",
                                   :select => "concept_name.concept_id,concept_name.name,obs.value_coded,obs.obs_datetime,obs.voided")
-  @patient = Person.find(:all,
+    @patient = Person.find(:all,
                            :joins => 
                                 "INNER JOIN obs ON 
                                  person.person_id = obs.person_id",
@@ -20,50 +24,51 @@ class ReportController < ApplicationController
                                             @start_date, @end_date],
                            :select => "person.voided,obs.value_coded,obs.obs_datetime,obs.voided ")
   
-  @times = []                         
+    @times = []                         
     @data_hash = Hash.new
-   start_date = @end_date.monday
+    start_date = @start_date
+    end_date = @end_date
 
-        while start_date > @start_date || start_date == @start_date
-          @times << start_date
-          start_date = 1.weeks.ago(start_date)
-          @end_date = 4.days.from_now(start_date)
+    while start_date >= @start_date and start_date <= end_date
+      @times << start_date
+      start_date = 1.weeks.from_now(start_date.monday)
+      end_date = 4.days.from_now(start_date)
+      if end_date > @end_date
+        end_date = @end_date
+      end
+    end
+    
+    @times.each{|t|
+      @diagnoses_hash = {}
+      patients = []
+      @patient.each{|p|
+        end_day = 4.days.from_now(t.monday)
+        if end_day > @end_date
+          end_day = @end_date
         end
-        @times = @times.reverse
-        #render :text => times.to_yaml and return
-
-              
-
-  #format data into array of times to be keys for @data_hash. @diagnosis_hashes will be the values
-     
-        @times.each{|t|
-          @diagnoses_hash = {}
-          patients = []
-          @patient.each{|p|
-          patients << p.value_coded if p.obs_datetime.to_date <= t and p.obs_datetime.to_date >= t.monday
-          }
-        #  render :text => patients.to_yaml and return
-        @diagnoses.each{|d|
+        patients << p if p.obs_datetime.to_date >= t and p.obs_datetime.to_date <= end_day
+      }
+      @diagnoses.each{|d|
         count = 0
-          patients.each{|patient|
-           count += 1  if patient == d.value_coded           
-        
+        patients.each{|patient|
+          count += 1  if patient.value_coded == d.value_coded
         }
         @diagnoses_hash[d.name] = count
-     }
-     #render :text => @diagnoses_hash.to_yaml and return
-     @data_hash["#{t}"] = @diagnoses_hash
-        }
-     #render :text => @data_hash.to_yaml and return
-        
-
-
+      }
+      @data_hash["#{t}"] = @diagnoses_hash
+    }
   end
 
   def aggregated_diagnosis
 
   @start_date = Date.new(params[:start_year].to_i,params[:start_month].to_i,params[:start_day].to_i) rescue nil
   @end_date = Date.new(params[:end_year].to_i,params[:end_month].to_i,params[:end_day].to_i) rescue nil
+   if @start_date > @end_date
+      flash[:notice] = 'Start date is greater that end date'
+      redirect_to :action => 'select'
+      return
+    end
+
   @diagnoses = ConceptName.find(:all,
                                   :joins =>
                                         "INNER JOIN obs ON
@@ -83,9 +88,17 @@ class ReportController < ApplicationController
   end
 
   def referral
-    @referrals = Observation.find(:all, :conditions => ["concept_id = ?", 2227])
+     @start_date = Date.new(params[:start_year].to_i,params[:start_month].to_i,params[:start_day].to_i) rescue nil
+    @end_date = Date.new(params[:end_year].to_i,params[:end_month].to_i,params[:end_day].to_i) rescue nil
+      if @start_date > @end_date
+        flash[:notice] = 'Start date is greater that end date'
+        redirect_to :action => 'select'
+        return
+      end
+
+    @referrals = Observation.find(:all, :conditions => ["concept_id = ? AND date_format(obs_datetime, '%Y-%m-%d') >= ? AND 
+                                  date_format(obs_datetime, '%Y-%m-%d') <= ?", 2227, @start_date, @end_date])
     @facilities = Observation.find(:all, :conditions => ["concept_id = ?", 2227], :group => "value_text")
-   # render:text => facilities.length and return
   end
 
   def report_date_select
