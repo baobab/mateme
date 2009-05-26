363ccfdc41c00c31ffaee09cd950ce8c2fd4257e
dave gamphani phiri <gamphani@kafyontha.bao.bab>
2009-05-26 09:59:48 +0200

    Improving interface and adding remote select options

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -57,6 +57,33 @@ class ReportController < ApplicationController
       }
       @data_hash["#{t}"] = @diagnoses_hash
     }
+
+    #Now create an array to use for sorting when we get to the view
+    @sort_array = []
+    sort_hash = {}
+
+    @diagnoses.each{|d|
+      sum = 0
+      @times.each{|t|
+        @data_hash.each{|time,data|
+          if t.to_date == time.to_date 
+            data.each{|k,v|
+            if k == d.name
+              sum = sum + v 
+            end
+          }
+          end
+      }
+
+
+    }
+    sort_hash[d.name] = sum
+
+    }
+
+  sort_hash = sort_hash.sort{|a,b| -1*( a[1]<=>b[1])}
+   sort_hash.each{|x| @sort_array << x[0]}
+
   end
 
   def aggregated_diagnosis
@@ -69,7 +96,8 @@ class ReportController < ApplicationController
       return
     end
 
-  @diagnoses = ConceptName.find(:all,
+  #getting an array of all diagnoses recorded within the chosen period - to avoid including existent but non recorded diagnoses
+  diagnoses = ConceptName.find(:all,
                                   :joins =>
                                         "INNER JOIN obs ON
                                          concept_name.concept_id = obs.value_coded",
@@ -77,6 +105,7 @@ class ReportController < ApplicationController
                                             @start_date, @end_date],
                                   :group =>   "name",
                                   :select => "concept_name.concept_id,concept_name.name,obs.value_coded,obs.obs_datetime,obs.voided")
+  #getting list of all patients who were diagnosed within the set period-to avoid getting all patients                          
   @patient = Person.find(:all,
                            :joins => 
                                 "INNER JOIN obs ON 
@@ -84,7 +113,34 @@ class ReportController < ApplicationController
                            :conditions => ["date_format(obs_datetime, '%Y-%m-%d') >= ? AND date_format(obs_datetime, '%Y-%m-%d') <= ?",
                                             @start_date, @end_date],
                            :select => "person.gender,person.birthdate,person.birthdate_estimated,person.date_created,
-                                      person.voided,obs.value_coded,obs.obs_datetime,obs.voided ") 
+                                      person.voided,obs.value_coded,obs.obs_datetime,obs.voided ")
+  
+  sort_hash = Hash.new
+
+  #sorting the diagnoses using frequency with the highest first
+  diagnoses.each{|diagnosis|
+    count = 0
+    @patient.each{|patient|
+      if patient.value_coded == diagnosis.value_coded
+        count += 1
+      end
+    }
+    sort_hash[diagnosis.name] = count
+  
+  }
+  #A sorted array of diagnoses to be sent to be sent to form
+  @diagnoses = Array.new
+
+   sort_hash = sort_hash.sort{|a,b| -1*( a[1]<=>b[1])}
+   diagnosis_names = []
+   sort_hash.each{|x| diagnosis_names << x[0]}
+   diagnosis_names.each{|d|
+     diagnoses.each{|diag|
+       @diagnoses << diag if d == diag.name     
+     }
+   }
+   
+
   end
 
   def referral
@@ -106,4 +162,26 @@ class ReportController < ApplicationController
   
   def select
   end
+  def select_remote_options
+    render :layout => false
+  end
+  def remote_report
+    s_day = params[:post]['start_date(3i)'].to_i #2
+    s_month = params[:post]['start_date(2i)'].to_i #12
+    s_year = params[:post]['start_date(1i)'].to_i  #2008
+    e_day = params[:post]['end_date(3i)'].to_i #18
+    e_month = params[:post]['end_date(2i)'].to_i #1
+    e_year = params[:post]['end_date(1i)'].to_i # 2009
+    parameters = {'start_year' => s_year, 'start_month' => s_month, 'start_day' => s_day,'end_year' => e_year, 'end_month' => e_month, 'end_day' => e_day}
+
+    if params[:report] == 'Weekly report'
+      redirect_to :action => 'weekly_report', :params => parameters
+    elsif params[:report] == 'Aggregated Diagnoses'
+      redirect_to :action => 'aggregated_diagnosis', :params => parameters
+    elsif params[:report] == 'Referrals'
+      redirect_to :action => 'referral', :params => parameters
+    end
+
+  end
+
 end
