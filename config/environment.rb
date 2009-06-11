99c002b3f3852f1753c499eea436ca0ad85bced1
Jeff Rafter <jeff@baobabhealth.org>
2009-06-11 09:05:37 -0700

    Specify rails env

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+# Specifies gem version of Rails to use when vendor/rails is not present
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
