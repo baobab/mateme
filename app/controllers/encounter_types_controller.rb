1d2902c0c1f9ef05da54de2acd4ae027d3808b8c
Jeff Rafter <jeff@baobabhealth.org>
2009-07-28 22:38:04 -0700

    Limit encounters to ones that are available *and* in the database

--- app/controllers/encounter_types_controller.rb
+++ app/controllers/encounter_types_controller.rb
@@ -2,9 +2,9 @@ class EncounterTypesController < ApplicationController
 
   def index
     # TODO add clever sorting
-    # @encounter_types = EncounterType.find(:all)
-
+    @encounter_types = EncounterType.find(:all).map{|enc|enc.name.gsub(/.*\//,"").gsub(/\..*/,"").humanize}
     @available_encounter_types = Dir.glob(RAILS_ROOT+"/app/views/encounters/*").map{|file|file.gsub(/.*\//,"").gsub(/\..*/,"").humanize}
+    @available_encounter_types -= @available_encounter_types - @encounter_types
   end
 
   def show
