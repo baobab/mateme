5a690184c4f95b6691329f5b7b88b6f2ce5def8a
Jeff Rafter <jeff@baobabhealth.org>
2009-08-03 12:26:21 -0700

    Lots of little barcodey things... better support for multiple barcodes per form for login and label support on the entry

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -3,18 +3,17 @@ class BarcodesController < ApplicationController
   end
 
   def create
-    @barcode = params[:barcode]
-    print_and_redirect("/barcodes/label?barcode=#{@barcode}", '/admin')  
+    print_and_redirect("/barcodes/label?barcode=#{params[:barcode]}&label=#{params[:label]}", '/admin')  
   end
 
   def label
-    @barcode = params[:barcode]    
     label = ZebraPrinter::StandardLabel.new
     label.font_size = 2
     label.font_horizontal_multiplier = 2
     label.font_vertical_multiplier = 2
     label.left_margin = 50
-    label.draw_barcode(50,180,0,1,5,15,120,false,"#{@barcode}")
+    label.draw_barcode(50,180,0,1,5,15,120,false,"#{params[:barcode]}")
+    label.draw_multi_text("#{(params[:label] || '').delete("'")}") #'
     data = label.print(1)
     send_data(data,
       :type=>"application/label; charset=utf-8", 
--- public/javascripts/barcode.js
+++ public/javascripts/barcode.js
@@ -2,25 +2,26 @@ var patnum = ""
 var setFocusTimeout = 5000;
 var checkForBarcodeTimeout = 1500;
 var barcodeFocusTimeoutId = null;
-
+var barcodeFocusOnce = false;
+var barcodeId = '';
 
 function loadBarcodePage() {
   focusForBarcodeInput()
   checkForBarcode()
 }
 
-function focusForBarcodeInput(barcodeId){
+function focusForBarcodeInput(){
 	if (!barcodeId) {
 		barcodeId = "barcode";
 	}
   var barcode = document.getElementById("barcode");
 	if (barcode) {
 		barcode.focus();
-		barcodeFocusTimeoutId = window.setTimeout("focusForBarcodeInput()", setFocusTimeout);
+		if (!focusOnce) barcodeFocusTimeoutId = window.setTimeout("focusForBarcodeInput()", setFocusTimeout);
 	}
 }
 
-function checkForBarcode(barcodeId, validAction){
+function checkForBarcode(validAction){
 	if (!barcodeId) {
 		barcodeId = "barcode";
 	}
@@ -31,16 +32,12 @@ function checkForBarcode(barcodeId, validAction){
   // Look for anything with a dollar sign at the end
   if (barcode_element.value.match(/.+\$$/i) != null){
     barcode_element.value = barcode_element.value.substring(0,barcode_element.value.length-1)
-
-		if (typeof validAction == "string" && barcodeId != "barcode")
-			eval(validAction);
-		else if (typeof barcodeScanAction != "undefined")
+		if (typeof barcodeScanAction != "undefined")
 			barcodeScanAction();
 		else
     	document.getElementById('barcodeForm').submit();
-    return
   }
-  window.setTimeout("checkForBarcode('"+barcodeId+"', '" + validAction + "')", checkForBarcodeTimeout);
+  window.setTimeout("checkForBarcode('" + validAction + "')", checkForBarcodeTimeout);
 }
 
 
