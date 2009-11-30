55995c36a3929a5cae01469b2b982756e27f5e8e
Dave Phiri <davegamphaniphiri@gmail.com>
2009-11-30 21:01:26 +0200

    Added hashes for arranging diagnoses in hierarchical form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -31,6 +31,8 @@ class SearchController < ApplicationController
 
   def diagnosis
 
+  if params[:location] == 'WARD 4B'
+
     syndromic_diagnoses = {
     'SYSTEMIC' => {'WASTING SYNDROME'=>{}, 'SEPSIS'=>{}, 'MALARIA' => {'UNCOMPLICATED'=>{}, 'SEVERE' =>{}}, 'SHOCK' => {'SEPTIC'=>{},'HAEMORRHAGIC'=>{}, 'UNEXPLAINED'=>{}},'MALNUTRITION'=>{},'OTHER SYSTEMIC CONDITION' =>{}},
 
@@ -68,6 +70,347 @@ class SearchController < ApplicationController
 'SKIN DISEASE' => {'HERPES ZOSTER'=>{}, 'HERPES SIMPLEX'=>{}, 'PYOGENIC INFECTION'=>{},
 'INFESTATION'=>{}, 'DRUG REACTION'=>{}, 'MALIGNANCY'=>{}, 'ALBINISM'=>{}, 'OTHER SKIN CONDITION' =>{} }
 } 
+
+  else
+     syndromic_diagnoses = {
+"SYSTEMIC" => { 
+	"WASTING SYNDROME" => {},
+	"SEPSIS" => {
+		"PRESUMED SEPSIS ON CLINICAL GROUNDS ONLY" => {},
+		"SEPSIS WITH PROVEN BACTERAEMIA" => {
+			"NON-TYPHI SALMONELLA (NTS) ISOLATED" => {},
+			"S PNEUMONIAE ISOLATED" => {},
+			"OTHER ORGANISM ISOLATED" => {}
+			}
+		},
+	"MALARIA" => {
+		"UNCOMPLICATED MALARIA" => {},
+		"SEVERE MALARIA" => {}
+		},
+	"SHOCK" => {
+		"SEPTIC SHOCK" => {},
+		"HAEMORRHAGIC SHOCK" => {},
+		"UNEXPLAINED SHOCK"  => {}
+		},
+	"MALNUTRITION" => {},
+	"OTHER SYSTEMIC DIAGNOSIS CONDITION" => {}
+	},
+	
+"NERVOUS SYSTEM" => {
+	"MENINGITIS" => {
+		"PYOGENIC MENINGITIS" => {
+			"S PNEUMONIAE PROVEN BY GRAM STAIN OR CULTURE" => {},
+			"H INFLUENZAE PROVEN BY CULTURE" => {},
+			"E COLI PROVEN BY CULTURE" => {},
+			"N MENINGITIDIS PROVEN BY GRAM STAIN OR CULTURE" => {},
+			"OTHER ORGANISM" => {},
+			"NO ORGANISM IDENTIFIED (CSF EXAMINED)" => {},
+			"CLINICAL DIAGNOSIS ONLY (CSF NOT EXAMINED)" => {}
+			},
+		"CRYPTOCOCCAL MENINGITIS" => {
+			"PROVEN BY INDIAN INK, CULTURE OR CRAG" => {},
+			"ASSUMED ON SYMPTOMS, CELL COUNT" => {}
+			},
+		"TUBERCULOUS MENINGITIS" => {
+			"PROVEN BY ZN OR CULTURE" => {},
+			"ASSUMED ON BASIS OF CLINICAL CONTEXT OR CSF CELL-COUNT" => {}
+			}
+		},
+	"RABIES" => {},
+	"OTHER ENCEPHALITIS"  => {},
+	"TETANUS" => {},
+	"STROKE" => {
+		"WITH BOTH HYPERTENSION AND DIABETES" => {},
+		"WITH HYPERTENSION (NO DIABETES)" => {},
+		"WITH DIABETES (NO HYPERTENSION)" => {},
+		"LIKELY EMBOLIC" => {},
+		"HIV THE ONLY RISK FACTOR" => {},
+		"OTHER IDENTIFIED CAUSE" => {},
+		"UNEXPLAINED" => {}
+		},
+	"PARAPLEGIA" => {
+		"TUBERCULOUS" => {},
+		"PYOGENIC" => {},
+		"TRANSVERSE MYELITIS" => {},
+		"SYPHILIS" => {},
+		"SCHISTOSOMIASIS" => {},
+		"NEOPLASTIC" => {},
+		"OTHER IDENTIFIED CAUSE" => {},
+		"UNEXPLAINED"  => {}
+		},
+	"INTRACEREBRAL SPACE-OCCUPYING LESION" => {
+		"TUMOUR" => {},
+		"ABSCESS" => {},
+		"TOXOPLASMOSIS" => {},
+		"HAEMATOMA" => {},
+		"OTHER IDENTIFIED LESION" => {},
+		"UNIDENTIFIED" => {}
+		},
+	"CONVULSIONS" => {
+		"KNOWN CHRONIC EPILEPSY" => {},
+		"RECENT ONSET, NOT EXPLAINED BY OTHER DIAGNOSIS" => {}
+		},
+	"PERIPHERAL NEUROPATHY" => {
+		"NUTRITIONAL" => {},
+		"DRUG-RELATED" => {},
+		"OTHER SPECIFIC CAUSE CONSIDERED LIKELY" => {},
+		"UNEXPLAINED" => {}
+		},
+	"PSYCHIATRIC SYNDROME" => {
+		"ACUTE CONFUSIONAL STATE" => {},
+		"CHRONIC PSYCHOSIS" => {},
+		"ANXIETY STATE" => {},
+		"DEPRESSION" => {},
+		"OTHER PSYCHIATRIC SYNDROME" => {}
+		},
+	"OTHER NERVOUS SYSTEM CONDITION" => {}
+	},
+
+"CARDIOVASCULAR" => {
+	"HEART FAILURE" => {
+		"CARDIOMYOPATHY" => {
+			"CARDIOMYOPATHY WITH UNKNOWN CAUSE" => {},
+			"PERI-PARTUM CARDIOMYOPATHY" => {},
+			"NUTRITIONAL CARDIOMYOPATHY" => {},
+			"ALCOHOLIC CARDIOMYOPATHY" => {},
+			"OTHER CARDIOMYOPATHY" => {}
+			},
+		"HEART FAILURE SECONDARY TO HYPERTENSION" => {},
+		"COR PULMONALE" => {},
+		"VALVULAR DISEASE" => {},
+		"OTHER CAUSE OF HEART FAILURE"  => {},
+		"UNEXPLAINED HEART FAILURE" => {}
+		},
+	"HYPERTENSION" => {
+		"ESSENTIAL HYPERTENSION" => {},
+		"MALIGNANT HYPERTENSION" => {},
+		"SECONDARY HYPERTENSION" => {
+			"RENAL DISEASE" => {},
+			"COARCTATION OF AORTA"  => {},
+			"ENDOCRINE SECONDARY HYPERTENSION" => {},
+			"OTHER SECONDARY HYPERTENSION" => {}
+			}
+		},
+	"PERICARDIAL EFFUSION" => {
+		"ASSUMED TUBERCULOUS PERICARDIAL EFFUSION" => {},
+		"PYOGENIC PERICARDIAL EFFUSION" => {},
+		"MALIGNANT PERICARDIAL EFFUSION" => {},
+		"URAEMIC PERICARDIAL EFFUSION" => {},
+		"OTHER PERICARDIAL EFFUSION" => {}
+		},
+	"ISCHAEMIC HEART DISEASE" => {},
+	"OTHER CARDIOVASCUALR CONDITION" => {}
+	},
+	
+"RESPIRATORY" => {
+	"PNEUMONIA" => {
+		"LOBAR" => {},
+		"BRONCHOPNEUMONIA" => {}
+		},
+	"ASTHMA"  => {},
+	"SMEAR POSITIVE TB" => {},
+	"SMEAR NEGATIVE TB" => {},
+	"PLEURAL EFFUSION" => {
+		"ASSUMED TB" => {},
+		"PARA-PNEUMONIC" => {},
+		"EMPYEMA" => {},
+		"KAPOSIS SARCOMA" => {},
+		"OTHER MALIGNANCY" => {},
+		"OTHER CAUSE" => {},
+		"TRANSUDATE"  => {}
+		},
+	"PNEUMOTHORAX" => {},
+	"LUNG ABSCESS" => {},
+	"PCP" => {},
+	"RESPIRATORY MALIGNANCY" => {
+		"KAPOSIS SARCOMA" => {},
+		"OTHER RESPIRATORYMALIGNANCY" => {}
+		},
+	"UPPER RESPIRATORY TRACT INFECTION" => {},
+	"BRONCHIECTASIS" => {},
+	"OTHER RESPIRATORY CONDITION" => {}
+	},
+
+"GASTROINTESTINAL" => {
+	"GASTROENTERITIS" => {
+		"ACUTE GASTROENTERITIS" => {},
+		"CHRONIC GASTROENTERITIS" => {}
+		},
+	"GI HAEMORRHAGE" => {
+		"BLEEDING PEPTIC ULCER"  => {},
+		"BLEEDING OESOPHAGEAL VARICES" => {},
+		"OTHER CAUSE OF GI BLEEDING" => {}
+		},
+	"PEPTIC ULCER DISEASE" => {},
+	"ACUTE PANCREATITIS" => {},
+	"ACUTE ABDOMINAL PAIN" => {},
+	"CANDIDIASIS" => {
+		"ORAL CANDIDIASIS" => {},
+		"OESOPHAGEAL CANDIDIASIS" => {}
+		},
+	"PALATAL KAPOSI" => {},
+	"ABDOMINAL MALIGNANCY" => {
+		"HEPATOCELLULAR CARCINOMA" => {},
+		"HEPATIC METASTASES" => {},
+		"PANCREATIC CARCINOMA" => {},
+		"OTHER ABDOMINAL TUMOUR" => {}
+		},
+	"HYPER-REACTIVE MALARIAL SPLENOMEGALY" => {},
+	"LIVER DISEASE" => {
+		"ACUTE HEPATITIS"  => {},
+		"OBSTRUCTIVE JAUNDICE" => {},
+		"PORTAL HYPERTENSION"  => {
+			"PORTAL HYPERTENSION DUE TO CIRRHOSIS" => {},
+			"PORTAL HYPERTENSION DUE TO SCHISTOSOMIASIS" => {}
+			},
+		"LIVER ABSCESS" => {
+			"AMOEBIC LIVER ABSCESS" => {},
+			"PYOGENIC LIVER ABSCESS" => {},
+			"OTHER LIVER DISEASE" => {}
+			}
+		},
+	"ASCITES" => {
+		"ASSUMED CIRRHOSIS" => {},
+		"ASSUMED TUBERCULOUS" => {},
+		"ASCITES WITH BACTERIAL INFECTION" => {}
+		},
+	"HELMINTH INFESTATION" => {
+		"HOOKWORM" => {},
+		"STRONGYLOIDES" => {},
+		"ASCARIASIS" => {},
+		"MANSONI" => {},
+		"CYSTICERCOSIS" => {},
+		"HYDATID" => {}
+		}
+	},
+
+"RENAL" => {
+	"RENAL FAILURE" => {
+		"ACUTE RENAL FAILURE" => {},
+		"CHRONIC RENAL FAILURE" => {}
+		},
+	"ACUTE NEPHRITIS" => {},
+	"NEPHROTIC SYNDROME" => {},
+	"URINARY TRACT INFECTION" => {},
+	"S HAEMATOBIUM" => {},
+	"STONE" => {
+		"KIDNEY STONE" => {},
+		"BLADDER STONE" => {}
+		},
+	"OTHER RENAL SYNDROME" => {},
+	"PROSTATIC DISEASE" => {
+		"BENIGN PROSTATIC HYPERTROPHY" => {},
+		"CARCINOMA OF PROSTATE" => {}
+		},
+	"TESTICULAR DISEASE" => {
+		"HYDROCELE" => {},
+		"TESTICULAR TUMOUR" => {}
+		},
+	"OBSTETRIC DISEASE" => {
+		"PELVIC INFLAMMATORY DISEASE" => {},
+		"OTHER GYNAECOLOGICAL DISEASE" => {}
+		}
+	},
+	
+"METABOLIC + ENDOCRINE + TOXINS/DRUGS" => {
+	"HYPOGLYCAEMIA" => {},
+	"DIABETES" => {
+		"HYPERGLYCAEMIA" => {},
+		"DKA" => {},
+		"HONK" => {}
+		},
+	"LACTIC ACIDOSIS" => {},
+	"THYROID" => {
+		"HYPERTHYROIDISM" => {},
+		"HYPOTHYROIDISM" => {}
+		},
+	"ADRENAL" => {
+		"CUSHINGS" => {},
+		"ADDISONS" => {}
+		},
+	"PITUITARY" => {
+		"HYPOPITUITARISM" => {},
+		"ACROMEGALY" => {},
+		"ADENOMA" => {}
+		},
+	"OTHER ENDOCRINE DISEASE" => {},
+	"POISONING" => {
+		"ORGANOPHOSPHATE" => {},
+		"CARBON MONOXIDE" => {},
+		"OTHER POISONING" => {},
+		"UNIDENTIFIED POISONING" => {}
+		},
+	"SYSTEMIC DRUG TOXICITY" => {
+		"DRUG REACTION" => {},
+		"DRUG OVERDOSE" => {},
+		"OTHER SYSTEMIC DRUG TOXICITY" => {}
+		}
+	},
+
+"HAEMATOLOGICAL /RETICULOENDOTHELIAL" => {
+	"ANAEMIA" => {
+		"MICROCYTIC" => {},
+		"MACROCYTIC" => {},
+		"PANCYTOPENIA" => {},
+		"OTHER KIND OF ANAEMIA" => {}
+		},
+	"LEUKAEMIA" => {},
+	"LYMPHOMA" => {},
+	"MYELOMA" => {},
+	"LYMPHADENOPATHY" => {
+		"TB LYMPHADENOPATHY" => {},
+		"LYMPHOMA" => {},
+		"OTHER LYMPHADENOPATHY" => {},
+		"UNEXPLAINED LYMPHADENOPATHY" => {}
+		},
+	"BLEEDING TENDENCY" => {
+		"BLEEDIND DUE TO IDIOPATHIC THROMBOCYTOPENIA" => {},
+		"BLEEDING DUE TO DIC" => {},
+		"BLEEDING DUE TO OTHER CAUSE" => {}
+		}
+	},
+
+"MUSCULOSKELETAL / DERMATOLOGICAL"  => {
+	"ARTHRITIS" => {
+		"ACUTE BACTERIAL" => {},
+		"ACUTE REACTIVE" => {},
+		"CHRONIC INFLAMMATORY" => {},
+		"OSTEOARTHROSIS" => {}
+		},
+	"SPONDYLITIS" => {
+		"TB	SPONDYLITIS" => {},
+		"PYOGENIC SPONDYLITIS" => {},
+		"NEOPLASTIC SPONDYLITIS" => {},
+		"SPONDYLITIS WITH UNIDENTIFIED CAUSE" => {}
+		},
+	"BONE MALIGNANCY" => {
+		"PRIMARY BONE MALIGNANCY" => {},
+		"METASTATIC BONE MALIGNANCY" => {}
+		},
+	"PYOMYOSITIS" => {},
+	"OSTEOMYELITIS"  => {},
+	"OTHER MUSCULOSKELETAL CONDITION"  => {},
+	"SKIN DISEASE" => {
+		"DERMATITIS" => {},
+		"PSORIASIS" => {},
+		"HERPES ZOSTER" => {},
+		"HERPES SIMPLEX" => {},
+		"PYOGENIC INFECTION" => {},
+		"INFESTATION" => {},
+		"DRUG REACTION" => {},
+		"MALIGNANCY" => {
+			"KAPOSIS SARCOMA" => {},
+			"MELANOMA" => {},
+			"BASAL CELL CARCINOMA"  => {},
+			"OTHER SKIN MALIGNANCY" => {}
+			},
+		"ALBINISM" => {},
+		"OTHER SKIN CONDITION" => {}
+		}
+	}
+}
+  end
+
 level = params[:level]
 selected = params[:selected]
 
@@ -95,6 +438,14 @@ elsif level == 'level_3'
       location.index(/#{search_string}/) || 100 # if the search string isn't found use value 100
     }[0..15]
 
+elsif level == 'level_4'
+  elements = []
+   syndromic_diagnoses.each{|k,v| v.each{|m,n| n.each{|key,value| value.each{|a,b| b.each{|y,z| elements << y if a == "#{selected}"}}}}}
+
+  @results = elements.grep(/#{search_string}/i).compact.sort_by{|location|
+      location.index(/#{search_string}/) || 100 # if the search string isn't found use value 100
+    }[0..15]
+
 else
 
 @results = syndromic_diagnoses.collect{|k,v| k}.grep(/#{search_string}/i).compact.sort_by{|location|
@@ -106,5 +457,16 @@ end
 render :text => @results.collect{|k,v|"<li>#{k}</li>"}.join("\n")
 
 end
+
+  def clinics
+    search_string = params[:search_string]
+    clinics = [  "QECH Medical clinic", " QECH Chest and Cardiac clinic", "QECH Neuro clinic", "QECH Diabetes clinic", "QECH Renal Clinic", "QECH ART Clinic", "QECH Surgical clinic", "QECH Obstetrics/gynaecology clinic", "QECH other", "QECH medical ward", "QECH medical teaching annex", "ART clinic at a centre other than QECH", "Clinic at another government hospital", "Private practitioner clinic", "Clinic at a private hospital"]
+
+    @results = clinics.grep(/#{search_string}/i).compact.sort_by{|location|
+      location.index(/#{search_string}/) || 100 # if the search string isn't found use value 100
+    }[0..15]
+
+    render :text => @results.collect{|clinic|"<li>#{clinic}</li>"}.join("\n")
+  end
 
 end
