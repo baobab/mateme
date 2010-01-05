ead6c6b7f170cdb6ef5ecc085add980d75b21197
Soyapi Mumba <soyapim@gmail.com>
2010-01-05 09:38:47 +0200

    Added Demographics Editing via Mastercard

--- app/controllers/person_names_controller.rb
+++ app/controllers/person_names_controller.rb
@@ -15,4 +15,24 @@ class PersonNamesController < ApplicationController
     @names = PersonNameCode.find_most_common(field_name, search_string).collect{|person_name| person_name.send(field_name)}
     render :text => "<li>" + @names.map{|n| n } .join("</li><li>") + "</li>"
   end
+
+  def edit
+    if request.get?
+      @patient = Patient.find(params[:id])
+      @patient_or_guardian = "patient"
+      @given_name = @patient.person.names.first.given_name
+      @family_name = @patient.person.names.first.family_name
+      render :layout => true
+    elsif request.post? && params[:given_name] && params[:family_name]
+      patient = Patient.find(params[:id])
+      patient.person.names.each{|patient_name|patient_name.void!('given another name')}
+
+	    person_name = PersonName.new
+	    person_name.given_name = params[:given_name]
+	    person_name.family_name = params[:family_name]
+	    person_name.person = patient.person
+	    person_name.save
+      redirect_to :controller => :patients, :action => :mastercard, :id => patient.id
+    end
+  end
 end
