599e1ffd6d1f0ea9cffe3ed024015dbb05222444
Dave Phiri <davegamphaniphiri@gmail.com>
2010-01-14 11:34:46 +0200

    Avoid readdmitting a patient who has already been admitted in current visit

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -182,7 +182,7 @@ class Patient < ActiveRecord::Base
   end
 
    def admitted_to_ward
-     self.encounters.all(:include => [:observations], :conditions => ["encounter.encounter_type = ?", EncounterType.find_by_name("ADMIT PATIENT").id]).map{|encounter| 
+     self.current_visit.encounters.all(:include => [:observations], :conditions => ["encounter.encounter_type = ?", EncounterType.find_by_name("ADMIT PATIENT").id]).map{|encounter| 
       encounter.observations.active.last(
         :conditions => ["obs.concept_id = ?", ConceptName.find_by_name("ADMIT TO WARD").concept_id])
      }.flatten.compact.last rescue nil
