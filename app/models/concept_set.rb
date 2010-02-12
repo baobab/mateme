cd9fdb292832a46d46e931c2172f3d3925de65c0
ceekays <edmond@baobabhealth.org>
2010-02-12 13:00:57 +0200

    Modified concept set mapping

--- app/models/concept_set.rb
+++ app/models/concept_set.rb
@@ -2,6 +2,6 @@ class ConceptSet < ActiveRecord::Base
   set_table_name :concept_set
   set_primary_keys :concept_id, :concept_set
   include Openmrs
-  belongs_to :set
+  belongs_to :set, :foreign_key => :concept_set, :class_name => "Concept"
   belongs_to :concept
 end
