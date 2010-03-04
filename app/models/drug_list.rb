5440b2357c2c9c3d5ca77a634a4c7d52355d2df2
ceekays <edmond@baobabhealth.org>
2010-03-04 17:04:27 +0200

    Added Diabetes Drug List Class that instantiates diabetes drug lists

---
+++ app/models/drug_list.rb
@@ -0,0 +1,111 @@
+class DrugList
+
+  def diabetes_drugs
+    @drugs ={
+    "CAPTOPRIL" => {
+      "CAPTOPRIL 6.25MG BD"   => {},
+      "CAPTOPRIL 6.25MG TDS"  => {},
+      "CAPTOPRIL 12.5MG BD"   => {},
+      "CAPTOPRIL 12.5MG TDS"  => {},
+      "CAPTOPRIL 25MG BD"     => {},
+      "CAPTOPRIL 25MG TDS"    => {},
+      "CAPTOPRIL 50MG BD"     => {},
+      "CAPTOPRIL 50MG TDS"    => {}
+    },
+    "GLIBENCLAMIDE" =>{
+      "GLIBENCLAMIDE 5MG OD"  => {},
+      "GLIBENCLAMIDE 5MG BD"  => {},
+      "GLIBENCLAMIDE 10MG OD" => {},
+      #"GLIBENCLAMIDE 10MG AM AND 5MG PM" =>{},
+      "GLIBENCLAMIDE 10MG BD" => {}
+    },
+
+    "METFORMIN" => {
+      "METFORMIN 500MG BD"    => {},
+      "METFORMIN 500MG OD"    => {},
+      "METFORMIN 500MG TDS"   => {},
+      "METFORMIN 750MG BD"    => {},
+      "METFORMIN 750MG OD"    => {},
+      "METFORMIN 750MG TDS"   => {},
+      "METFORMIN 850MG BD"    => {},
+      "METFORMIN 850MG OD"    => {},
+      "METFORMIN 850MG TDS"   => {},
+      "METFORMIN 1000MG BD"   => {},
+      "METFORMIN 1000MG OD"   => {},
+      "METFORMIN 1000MG TDS"  => {}
+
+    },
+
+    "QUINAPRIL" =>{
+      "QUINAPRIL 20MG OD" =>{},
+      "QUINAPRIL 40MG OD" =>{}
+    },
+
+    "ASPIRIN" => {
+      "ASPIRIN 75MG OD" =>{},
+      "ASPIRIN 150MG OD" =>{}
+    },
+
+    "INSULIN, LENTE" => {
+      "LENTE INSULIN BD" =>{},
+      "LENTE INSULIN OD" =>{}
+      },
+    "INSULIN, REGULAR" => {
+      "SOLUBLE INSULIN BD" =>{},
+      "SOLUBLE INSULIN OD" =>{},
+      "SOLUBLE INSULIN TDS"=>{}
+
+    },
+
+     "HCT" => {
+      "HCT 12.5MG OD" => {},
+      "HCT 25MG OD" => {}
+    },
+
+  "NIFEDIPINE" => {
+    "NIFEDIPINE 10MG TDS" => {},
+    "NIFEDIPINE 20MG TDS" => {}
+  },
+
+  "NIFEDIPINE SR" => {
+    "NIFEDIPINE SR 10MG BD" => {},
+    "NIFEDIPINE SR 20MG BD" => {}
+  },
+
+  "PROPANOLOL" => {
+    "PROPANOLOL 80MG BD" => {},
+    "PROPANOLOL 80MG TDS" => {},
+    "PROPANOLOL 160MG BD" => {},
+    "PROPANOLOL 160MG TDS" => {},
+    "PROPANOLOL 240MG BD" => {},
+    "PROPANOLOL 240MG TDS" => {},
+    "PROPANOLOL 320MG BD" => {},
+    "PROPANOLOL 320MG TDS" => {}
+  },
+
+  "METHYLDOPA" => {
+    "METHYLDOPA 125MG BD" => {},
+    "METHYLDOPA 125MG TDS" => {},
+    "METHYLDOPA 250MG BD" => {},
+    "METHYLDOPA 250MG TDS" => {},
+    "METHYLDOPA 500MG BD" => {},
+    "METHYLDOPA 500MG TDS" => {}
+  },
+
+  "FUROSEMIDE" => {
+    "FUROSEMIDE 120MG BD" => {},
+    "FUROSEMIDE 120MG TDS" => {},
+    "FUROSEMIDE 20MG BD" => {},
+    "FUROSEMIDE 20MG TDS" => {},
+    "FUROSEMIDE 40MG BD" => {},
+    "FUROSEMIDE 40MG TDS" => {},
+    "FUROSEMIDE 60MG BD" => {},
+    "FUROSEMIDE 60MG TDS" => {},
+    "FUROSEMIDE 80MG BD" => {},
+    "FUROSEMIDE 80MG TDS" => {}
+  }
+  }
+  end
+  
+end
+
