1d956c91698cb586560841340b255b4726995f47
kachaje <chimwemwe.kachaje@baobabhealth.org>
2010-04-20 15:54:59 +0200

    Added some routes to handle routing error on the production server

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ ActionController::Routing::Routes.draw do |map|
   map.admin  '/admin',  :controller => 'admin', :action => 'index'
   map.login  '/login',  :controller => 'sessions', :action => 'new'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login  '/people/login',  :controller => 'sessions', :action => 'new'
+  map.logout '/people/logout', :controller => 'sessions', :action => 'destroy'
   map.location '/location', :controller => 'sessions', :action => 'location'
   map.resource :session
   map.resources :barcodes, :collection => {:label => :get}
