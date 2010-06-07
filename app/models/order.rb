530849f45791b9ea540b617907e62f3f2d64e054
ceekays <kachaleedmond@gmail.com>
2010-06-07 16:07:55 +0200

    Added methods for querying drug formulation and medication duration for a given patient

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,44 +14,79 @@ class Order < ActiveRecord::Base
   def to_s
     "#{drug_order}"
   end
-end
 
-# CREATE TABLE `orders` (
-#  `order_id` int(11) NOT NULL AUTO_INCREMENT,
-#  `order_type_id` int(11) NOT NULL DEFAULT '0',
-#  `concept_id` int(11) NOT NULL DEFAULT '0',
-#  `orderer` int(11) DEFAULT '0',
-#  `encounter_id` int(11) DEFAULT NULL,
-#  `instructions` text,
-#  `start_date` datetime DEFAULT NULL,
-#  `auto_expire_date` datetime DEFAULT NULL,
-#  `discontinued` tinyint(1) NOT NULL DEFAULT '0',
-#  `discontinued_date` datetime DEFAULT NULL,
-#  `discontinued_by` int(11) DEFAULT NULL,
-#  `discontinued_reason` int(11) DEFAULT NULL,
-#  `creator` int(11) NOT NULL DEFAULT '0',
-#  `date_created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-#  `voided` tinyint(1) NOT NULL DEFAULT '0',
-#  `voided_by` int(11) DEFAULT NULL,
-#  `date_voided` datetime DEFAULT NULL,
-#  `void_reason` varchar(255) DEFAULT NULL,
-#  `patient_id` int(11) NOT NULL,
-#  `accession_number` varchar(255) DEFAULT NULL,
-#  PRIMARY KEY (`order_id`),
-#  KEY `order_creator` (`creator`),
-#  KEY `orderer_not_drug` (`orderer`),
-#  KEY `orders_in_encounter` (`encounter_id`),
-#  KEY `type_of_order` (`order_type_id`),
-#  KEY `user_who_discontinued_order` (`discontinued_by`),
-#  KEY `user_who_voided_order` (`voided_by`),
-#  KEY `discontinued_because` (`discontinued_reason`),
-#  KEY `order_for_patient` (`patient_id`),
-#  CONSTRAINT `discontinued_because` FOREIGN KEY (`discontinued_reason`) REFERENCES `concept` (`concept_id`),
-#  CONSTRAINT `orderer_not_drug` FOREIGN KEY (`orderer`) REFERENCES `users` (`user_id`),
-#  CONSTRAINT `orders_in_encounter` FOREIGN KEY (`encounter_id`) REFERENCES `encounter` (`encounter_id`),
-#  CONSTRAINT `order_creator` FOREIGN KEY (`creator`) REFERENCES `users` (`user_id`),
-#  CONSTRAINT `order_for_patient` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`) ON UPDATE CASCADE,
-#  CONSTRAINT `type_of_order` FOREIGN KEY (`order_type_id`) REFERENCES `order_type` (`order_type_id`),
-#  CONSTRAINT `user_who_discontinued_order` FOREIGN KEY (`discontinued_by`) REFERENCES `users` (`user_id`),
-#  CONSTRAINT `user_who_voided_order` FOREIGN KEY (`voided_by`) REFERENCES `users` (`user_id`)
-# ) ENGINE=InnoDB AUTO_INCREMENT=3415 DEFAULT CHARSET=utf8 
+  def self.treatement_orders(patient_id)
+
+    treatment_encouter_id   = EncounterType.find_by_name("TREATMENT").id
+    drug_order_id           = OrderType.find_by_name("DRUG ORDER").id
+    diabetes_id             = Concept.find_by_name("DIABETES MEDICATION").id
+    hypertensition_id       = Concept.find_by_name("HYPERTENSION").id
+
+    self.find_by_sql("SELECT distinct orders.order_id, orders.concept_id,concept_name.name AS drug_name,obs.value_coded AS diagnosis_id,
+                         MAX(auto_expire_date) AS end_date, MIN(start_date) AS start_date,
+                         DATEDIFF(MAX(auto_expire_date), MIN(start_date))AS days,
+                         DATEDIFF(NOW(), MIN(start_date)) days_so_far,
+                        dose, drug.units, frequency
+                        FROM obs
+                        INNER JOIN encounter on encounter.encounter_id = obs.encounter_id
+                        INNER JOIN orders on orders.encounter_id = encounter.encounter_id
+                        INNER JOIN concept_name ON concept_name.concept_id = orders.concept_id
+                        INNER JOIN drug_order ON drug_order.order_id = orders.order_id
+                        INNER JOIN drug ON drug.drug_id = drug_order.drug_inventory_id
+                        INNER JOIN concept_name_tag_map on concept_name_tag_map.concept_name_id = concept_name.concept_name_id
+                        WHERE encounter_type = #{treatment_encouter_id} AND encounter.patient_id = #{patient_id}
+                          AND encounter.voided = 0 AND orders.voided = 0
+                          AND orders.order_type_id = #{drug_order_id} AND obs.value_coded IN (#{diabetes_id}, #{hypertensition_id})
+                          AND concept_name_tag_id = 4
+                        GROUP BY order_id, obs.value_coded
+                        ORDER BY drug_name, start_date DESC")
+  end
+
+  def self.aggregate_treatement_orders(patient_id)
+
+    hypertensition_medication_id  = Concept.find_by_name("HYPERTENSION MEDICATION").id
+    treatment_encouter_id         = EncounterType.find_by_name("TREATMENT").id
+    drug_order_id                 = OrderType.find_by_name("DRUG ORDER").id
+    diabetes_id                   = Concept.find_by_name("DIABETES MEDICATION").id
+    hypertensition_id             = Concept.find_by_name("HYPERTENSION").id
+    preffered_id                  = ConceptNameTag.find_by_tag("PREFERRED").id
+
+    medication_query = "SELECT medication.drug_name AS drug_name,
+      medication.days                             AS days,
+      medication.units                            AS units,
+      medication.dose                             AS formulation,
+      SUM(medication.days_so_far)                 AS total_medication_days,
+      MIN(medication.start_date)                  AS start_date,
+      MAX(medication.end_date)                    AS end_date,
+      medication.diagnosis_id                     AS diagnosis_id,
+      DATEDIFF(NOW(), MIN(medication.start_date)) AS duration
+      FROM (
+        SELECT auto_expire_date AS end_date, concept_name.name AS drug_name,
+          DATEDIFF(auto_expire_date, MIN(start_date)) AS days,
+          DATEDIFF(NOW(), start_date) days_so_far,start_date AS start_date,
+          dose, drug.units, frequency, obs.value_coded AS diagnosis_id,
+          orders.concept_id AS concept_id, orders.order_id AS order_id
+          FROM obs, encounter, orders, concept_name,drug_order, drug, concept_name_tag_map
+          WHERE encounter_type        = #{treatment_encouter_id}
+            AND encounter.patient_id  = #{patient_id}
+            AND encounter.voided = 0
+            AND orders.voided    = 0
+            AND orders.order_type_id = #{drug_order_id}
+            AND obs.value_coded IN (#{diabetes_id}, #{hypertensition_id}, #{hypertensition_medication_id})
+            AND concept_name_tag_id = #{preffered_id}
+            AND orders.encounter_id = encounter.encounter_id
+            AND encounter.encounter_id  = obs.encounter_id
+            AND concept_name.concept_id = orders.concept_id
+            AND drug_order.order_id     = orders.order_id
+            AND drug.drug_id = drug_order.drug_inventory_id
+            AND concept_name_tag_map.concept_name_id = concept_name.concept_name_id
+          GROUP BY auto_expire_date, 	concept_name.name, dose, drug.units, frequency,
+                obs.value_coded, 	orders.concept_id, orders.order_id, start_date
+          ORDER BY drug_name, start_date DESC) AS medication
+      WHERE medication.end_date >= NOW()
+      GROUP BY drug_name
+      ORDER BY drug_name"
+
+    self.find_by_sql(medication_query);
+  end
+end
