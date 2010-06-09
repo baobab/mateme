3c1d22e39177b27779d1ae6b17579b625cf665dd
kachaje <chimwemwe.kachaje@baobabhealth.org>
2010-06-09 15:55:48 +0200

    Added feature to login using barcodes

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,4 +67,81 @@ class User < ActiveRecord::Base
     end
   end
 
+  def username_hash(login)
+    u = User.find(:first, :conditions => {:username => login})
+    user_salt = u.salt.scan(/./)[0..2]
+    secret_name = GlobalProperty.find_by_property('server.secret_name').property_value.scan(/./)[0..2]
+    user_name = login.scan(/./)
+    space = GlobalProperty.find_by_property('server.secret_name_space').property_value.to_i
+
+    intermediate = Array.new(user_salt.length * space + secret_name.length * space)
+
+
+    salt_position = 0
+    secret_name_position = 0
+
+    length = 0
+    for length in 0..intermediate.length - 1
+      if salt_position < user_salt.length && length.even?
+        intermediate[length] = user_salt[salt_position]
+        salt_position += 1
+      elsif secret_name_position < secret_name.length && length.odd?
+        intermediate[length] = secret_name[secret_name_position]
+        secret_name_position +=1
+      end
+    end
+    intermediate.compact!
+
+    last = Array.new(intermediate.length * space + user_name.length * space)
+
+
+    length = 0
+    user_name_position = 0
+    intermediate_position = 0
+
+    for length in 0..last.length-1
+      if user_name_position < user_name.length && length.odd?
+        last[length] = user_name[user_name_position]
+        user_name_position += 1
+      elsif intermediate_position < intermediate.length && length.even?
+        last[length] = intermediate[intermediate_position]
+        intermediate_position += 1
+      end
+    end
+
+    last.compact!
+    return last
+
+
+  end
+
+  def self.decode_hash(login_barcode)
+    space = GlobalProperty.find_by_property('server.secret_name_space').property_value.to_i
+    login = ''
+    login_array = login_barcode.scan(/./)
+    position = space - 1
+    6.times{
+      login += login_array[position] rescue nil
+      position += space
+
+    }
+    return User.find(:first, :conditions => {:username => login})
+  end
+
+  def login_barcode
+    barcode_to_print = self.username_hash(self.username) rescue nil
+    return unless barcode_to_print
+    barcode_hash = barcode_to_print.to_s
+    label = ZebraPrinter::StandardLabel.new
+    label.font_size = 2
+    label.font_horizontal_multiplier = 2
+    label.font_vertical_multiplier = 2
+    label.left_margin = 50
+    label.draw_barcode(50,180,0,1,3,12,120,false,"#{barcode_hash}")
+    label.draw_multi_text("#{self.name.titleize}")
+    label.draw_multi_text('QECH')
+    label.draw_multi_text(' ')
+    label.print(1)
+  end
+
  end
