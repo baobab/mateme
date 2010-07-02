49c0515fb83907e33aab69934657dc7d68e33e9d
Dave Phiri <davegamphaniphiri@gmail.com>
2010-07-02 16:02:19 +0200

    Changed field names to reflect usage and references

--- db/migrate/20100510073658_create_location_drugs.rb
+++ db/migrate/20100510073658_create_location_drugs.rb
@@ -1,9 +1,9 @@
 class CreateLocationDrugs < ActiveRecord::Migration
   def self.up
     create_table :location_drugs do |t|
-      t.integer :drug_id
-      t.string :drug_name
-      t.integer :in_stock
+      t.integer :drug_concept_id
+      t.string :drug_concept_name
+      t.integer :in_stock, :default => 1
       t.integer :created_by
       t.timestamps
     end
