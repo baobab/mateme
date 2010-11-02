d97587587e8a8202116460ebc9af3cc2b2e37325
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2010-11-02 10:07:11 +0200

    Remodified the code for user role checking

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,7 +8,10 @@ class AdminController < ApplicationController
 private
   
   def require_admin
-    unless current_user.admin.downcase? || current_user.superuser.downcase?
+    # unless current_user.admin.downcase? || current_user.superuser.downcase?
+
+    roles = current_user.user_roles.collect{|r| r.role.downcase}
+    unless roles.include?("admin") || roles.include?("superuser")
       flash[:error] = "You must be an admin to view the admin page"
       redirect_to '/'
     end  
