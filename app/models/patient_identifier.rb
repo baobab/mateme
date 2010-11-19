a1dfcea5fb57f1227fb9b87bc33471b7141bcbbf
gamphani <dave.phiri@baobabhealth.org>
2010-11-19 09:18:53 +0200

    Introducing lab id identifier

--- app/models/patient_identifier.rb
+++ app/models/patient_identifier.rb
@@ -26,4 +26,28 @@ class PatientIdentifier < ActiveRecord::Base
     checkdigit = checkdigit +1 while ((sum+(checkdigit))%10)!=0
     return checkdigit
   end
+
+  # lab_id: is a function used to calculate the next free available
+  # LAB TEST SERIAL NUMBER, where prefix is the prefix appended to the Lab Identifiers
+  def lab_id=(prefix)
+    id = PatientIdentifier.find_by_sql("SELECT COALESCE(MAX(CONVERT(\
+            SUBSTRING(identifier, 4), UNSIGNED)),0) identifier \
+            FROM patient_identifier p LEFT OUTER JOIN patient_identifier_type t \
+            ON t.patient_identifier_type_id = p.identifier_type \
+            WHERE name = 'LAB IDENTIFIER'")[0].identifier.to_i + 1
+
+    self.identifier = "#{prefix}#{id}"
+  end
+
+  # identifier_type_name: is a method used to get the identifier_type for a
+  # patient identifier, in which case the name of the identifier type is parsed
+  # to obtain it's primary key which is returned
+  def identifier_type_name=(identifier_type_name)
+    identifier = PatientIdentifierType.find_by_name(identifier_type_name) rescue nil
+
+    raise "#{identifier_type_name} not a valid identifier_type" if identifier.nil?
+
+    self.identifier_type = identifier.patient_identifier_type_id
+  end
+
 end
