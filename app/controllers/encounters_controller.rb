4f1fc92b8804d84ebf45020550de96fefe64bcbd
ceekays <kachaleedmond@gmail.com>
2010-11-19 13:18:14 +0200

    Disabled the Hypertension management 'button'

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -266,7 +266,7 @@ class EncountersController < ApplicationController
           'Hospital Admissions',
           'Past Diabetes Medical History',
           'Complications',
-          'Hypertension Management',
+          #'Hypertension Management',
           'General Health'
         ]
 
