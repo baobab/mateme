1ce412b3135bcb3a971d2d36db0d08a489ce430e
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2011-03-16 10:43:12 +0200

    Modified the code for DC Number generation in response to a bug reported on issue #999

--- app/models/global_property.rb
+++ app/models/global_property.rb
@@ -4,5 +4,22 @@ class GlobalProperty < ActiveRecord::Base
   set_primary_key "id"
   def to_s
     return "#{property}: #{property_value}"
-  end  
+  end
+
+  def self.increment
+    position = self.find_by_property("dc.number.autoincrement")
+
+    if(position)
+      position.update_attribute("property_value", position.property_value.to_i + 1)
+
+      return position.property_value
+    else
+      return 0
+    end
+  end
+
+  def self.prefix
+    self.find_by_property("dc.number.prefix").property_value rescue ""
+  end
+  
 end
---
+++ db/migrate/create_dc_autoincrement_index.sql
@@ -0,0 +1,3 @@
+SET FOREIGN_KEY_CHECKS=0;
+INSERT INTO global_property (property, property_value, `description`) VALUES ('dc.number.autoincrement', 0, 'The autoincrement last position for Diabtes Clinic Numbers');
+SET FOREIGN_KEY_CHECKS=1;
