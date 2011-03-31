1d852b35b571aa46d1975d2f066eebd8ae817d90
chawanangwa <chawanangwa.lupafya@baobabhealth.org>
2011-03-31 16:28:27 +0200

    change the rails varsion to 2.3.2

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
-#Commented out rails version 2.3.2 so as to ensure that the system uses the latest version of rails installed on the machine.
-#RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 MATEME_VERSION = '1.2'
 
