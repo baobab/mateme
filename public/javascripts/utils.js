1fb9c93b255fee260ef611b12dfea7569e08121b
Precious <precious.bondwe@googlemail.com>
2011-04-14 15:24:55 +0200

    Standardisation changes

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -11,11 +11,54 @@ function addOnMouseDownAction(optionText){
   for(var i = 0; i < choices.length; i++){
     var onMouseDown = choices[i].getAttribute('onmousedown');
 
-    if(choices[i].innerHTML == optionText){
-      choices[i].setAttribute('onmousedown', onMouseDown + " setNextButtonText('Finish')");
+    if((optionText.join(';')+';').match(choices[i].innerHTML +';')){
+      choices[i].setAttribute('onmousedown', onMouseDown + " setNextButtonText('Finish');");
     }
     else{
-      choices[i].setAttribute('onmousedown', onMouseDown + " setNextButtonText('Next')");
+      choices[i].setAttribute('onmousedown', onMouseDown + " setNextButtonText('Next');");
     }
   }
 }
+
+/* format date by either estimating it or giving it a valid format*/
+function formatDate(main_date, sub_str, pos){
+
+  var formatted_date = [];
+
+  formatted_date['value_modifier'] = ''; /*assume the date is not estimated*/
+
+  if(pos == 0) /*year*/
+    {
+      if (sub_str.toLowerCase() != 'unknown') {
+          formatted_date['value_datetime'] = sub_str;
+        }
+     else{
+        formatted_date['value_datetime'] = null; /* since year is unknown, leave the date 'null'*/
+      }
+    }
+
+  else if(pos == 1) /*month*/
+    {
+
+      if (sub_str.toLowerCase() != 'unknown') {
+          formatted_date['value_datetime'] = main_date +'-'+ sub_str;
+        }
+     else{
+        formatted_date['value_datetime'] = main_date +'-07-01'; /* since month is unknown, estimate date*/
+        formatted_date['value_modifier'] = 'ES';
+      }
+    }
+
+  else if(pos == 2) /*day*/
+    {
+      if (sub_str.toLowerCase() != 'unknown') {
+           formatted_date['value_datetime'] = main_date +'-'+ sub_str;
+        }
+     else{
+        formatted_date['value_datetime'] = main_date +'-15'; /* since day is unknown, estimate date*/
+        formatted_date['value_modifier'] = 'ES';
+      }
+    }
+
+  return formatted_date;
+ }
