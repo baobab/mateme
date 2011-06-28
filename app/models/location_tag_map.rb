cbe3f3e57bb29ad0fce25f516c015c50f3e63eea
chawanangwa <chawanangwa.lupafya@baobabhealth.org>
2011-06-28 10:27:50 +0200

    set table primary key

--- app/models/location_tag_map.rb
+++ app/models/location_tag_map.rb
@@ -1,4 +1,4 @@
 class LocationTagMap < ActiveRecord::Base
   set_table_name "location_tag_map"
-  set_primary_key "location_tag_id, location_id"
+  set_primary_keys :location_tag_id, :location_id
 end
