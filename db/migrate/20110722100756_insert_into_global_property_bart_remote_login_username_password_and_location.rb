f91a47e5f2b2f4f462e5051d01efcf7290396c8d
chawanangwa <chawanangwa.lupafya@baobabhealth.org>
2011-07-22 13:30:55 +0200

    added global property for login, password and location for login to Bart

---
+++ db/migrate/20110722100756_insert_into_global_property_bart_remote_login_username_password_and_location.rb
@@ -0,0 +1,18 @@
+class InsertIntoGlobalPropertyBartRemoteLoginUsernamePasswordAndLocation < ActiveRecord::Migration
+  def self.up
+      execute "INSERT INTO `global_property` (`property`, `property_value`, `description`) 
+               VALUES ('remote_bart.username', 'admin','Username for login to the remote system e.g in this case either Bart 1 or 2');"
+
+      execute "INSERT INTO `global_property` (`property`, `property_value`, `description`) 
+               VALUES ('remote_bart.password','test', 'Password for login to the remote system e.g in this case either Bart 1 or 2');"
+               
+      execute "INSERT INTO `global_property` (`property`, `property_value`, `description`)
+               VALUES ('remote_bart.location',31, 'Location for login to the remote system e.g in this case either Bart 1 or 2');"
+  end
+
+  def self.down
+      execute "DELETE FROM `global_property` WHERE property = 'remote_bart.username'"
+      execute "DELETE FROM `global_property` WHERE property = 'remote_bart.password'"
+      execute "DELETE FROM `global_property` WHERE property = 'remote_bart.location'"
+  end
+end
