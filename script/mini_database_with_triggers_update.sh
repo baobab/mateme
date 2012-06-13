c21de581c38a39c4092f02f5f3f510ff8d3f1860
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2012-06-13 09:26:20 +0200

    Added script to initialise triggers on a database

---
+++ script/mini_database_with_triggers_update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+usage(){
+  echo "Usage: $0 ENVIRONMENT SITE"
+  echo
+  echo "ENVIRONMENT should be: development|test|production"
+  echo "Available SITES:"
+  ls -1 db/data
+} 
+
+ENV=$1
+SITE=$2
+
+if [ -z "$ENV" ] || [ -z "$SITE" ] ; then
+  usage
+  exit
+fi
+
+set -x # turns on stacktrace mode which gives useful debug information
+
+if [ ! -x config/database.yml ] ; then
+   cp config/database.yml.example config/database.yml
+fi
+
+USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['database']"`
+
+mysqldump --user=$USERNAME --password=$PASSWORD $DATABASE --no-data > tmp/schema.sql
+mysqldump --user=$USERNAME --password=$PASSWORD $DATABASE --no-create-info --skip-triggers > tmp/data.sql
+mysql --user=$USERNAME --password=$PASSWORD -e "DROP DATABASE ${DATABASE};"
+mysql --user=$USERNAME --password=$PASSWORD -e "CREATE DATABASE ${DATABASE};"
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < tmp/schema.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/triggers/patient_report.sql
+
+FILES=db/triggers/*
+for f in $FILES
+do
+	echo "Installing $f trigger file..."
+	mysql --user=$USERNAME --password=$PASSWORD $DATABASE < $f
+done
+
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < tmp/data.sql
+
+rm tmp/schema.sql
+rm tmp/data.sql
+
